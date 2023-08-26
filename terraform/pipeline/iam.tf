890a05be0c10a8b2162fed0a96a6e67030af6852
kazshi01 <blumedio0619@gmail.com>
2023-08-26 10:50:37 +0900

    modify

--- terraform/pipeline/iam.tf
+++ terraform/pipeline/iam.tf
@@ -1,13 +1,13 @@
 # IAM Role for CodeBuild
 resource "aws_iam_role" "codebuild_role" {
-  name = "example-role"
+  name = "codebuild-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
         Principal = {
           Service = "codebuild.amazonaws.com"
         }
@@ -21,23 +21,23 @@ resource "aws_iam_role" "codepipeline_role" {
   name = "codepipeline-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
         Action = "sts:AssumeRole",
         Effect = "Allow",
         Principal = {
-          Service = "codepipeline.amazonaws.com",
-        },
-      },
-    ],
+          Service = "codepipeline.amazonaws.com"
+        }
+      }
+    ]
   })
 }
 
 data "aws_iam_policy_document" "codepipeline_s3_policy" {
   statement {
     actions   = ["s3:PutObject"]
-    resources = ["arn:aws:s3:::self-dev-marukome/*"]
+    resources = ["arn:aws:s3:::self-dev-marukome/codepipeline/fargate/*"]
   }
 }
 
@@ -50,4 +50,33 @@ resource "aws_iam_role_policy_attachment" "codepipeline_s3_attachment" {
   policy_arn = aws_iam_policy.codepipeline_s3.arn
   role       = aws_iam_role.codepipeline_role.name
 }
+
+resource "aws_iam_policy" "codepipeline_policy" {
+  name        = "codepipeline-policy"
+  description = "Policy for CodePipeline to access CodeCommit and start CodeBuild"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "codecommit:GetBranch",
+          "codecommit:GetCommit",
+          "codecommit:UploadArchive",
+          "codecommit:GetUploadArchiveStatus",
+          "codecommit:CancelUploadArchive",
+          "codebuild:StartBuild", # Add this action to allow CodePipeline to start CodeBuild
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline_policy_attachment" {
+  policy_arn = aws_iam_policy.codepipeline_policy.arn
+  role       = aws_iam_role.codepipeline_role.name
+}
+
 
