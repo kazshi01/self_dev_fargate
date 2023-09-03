d3df30a98f81fd7821a3cd4d0ab22642c50e9838
kazshi01 <blumedio0619@gmail.com>
2023-09-03 18:25:08 +0900

    modify

--- terraform/fargate_pipeline/lambda.tf
+++ terraform/fargate_pipeline/lambda.tf
@@ -6,4 +6,7 @@ resource "aws_lambda_function" "slack_approval" {
 
   filename         = "./slack_notifier/slack_function.zip"                   # 手元のZIPファイルのパス
   source_code_hash = filebase64sha256("./slack_notifier/slack_function.zip") # ソースコードが変更されたときにのみ更新をトリガーするためのパラメータ
+
+  timeout     = 30  # タイムアウトを30秒に設定
+  memory_size = 256 # メモリサイズを256MBに設定
 }
