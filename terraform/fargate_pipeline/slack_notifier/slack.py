122810a13a42937d491260f78f665f9029e6a237
kazshi01 <blumedio0619@gmail.com>
2023-09-09 09:58:00 +0900

    modify

--- terraform/fargate_pipeline/slack_notifier/slack.py
+++ terraform/fargate_pipeline/slack_notifier/slack.py
@@ -1,7 +1,3 @@
-import json
-import urllib.request
-import boto3
-
 def lambda_handler(event, context):
     client = boto3.client('codepipeline')
 
@@ -13,27 +9,7 @@ def lambda_handler(event, context):
         # Slack に承認が必要な旨を通知
         slack_url = "https://hooks.slack.com/services/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"
         message = {
-            "text": "承認が必要です",
-            "attachments": [
-                {
-                    "text": "承認しますか？",
-                    "callback_id": job_id,  # CodePipeline の job ID を callback_id として保存
-                    "actions": [
-                        {
-                            "name": "approve",
-                            "text": "承認",
-                            "type": "button",
-                            "value": approval_token  # 承認用トークンを value として保存
-                        },
-                        {
-                            "name": "reject",
-                            "text": "却下",
-                            "type": "button",
-                            "value": approval_token  # 承認用トークンを value として保存
-                        }
-                    ]
-                }
-            ]
+            "text": "ビルドフェーズが完了しました。手動承認が必要です。"
         }
         
         request = urllib.request.Request(
@@ -48,42 +24,9 @@ def lambda_handler(event, context):
         except Exception as e:
             print("Error occurred:", e)
         
-    # Slack からの event の場合
-    elif 'body' in event:
-        slack_event = json.loads(event['body'])
-        action = slack_event['actions'][0]['name']
-        approval_token = slack_event['actions'][0]['value']
-        job_id = slack_event['callback_id']
-
-        if action == 'approve':
-            client.put_approval_result(
-                pipelineName='fargate-pipeline',
-                stageName='InvokeLambdaBeforeApproval',
-                actionName='Approval',
-                result={
-                    'summary': 'Approved from Slack',
-                    'status': 'Approved'
-                },
-                token=approval_token
-            )
-        elif action == 'reject':
-            client.put_approval_result(
-                pipelineName='fargate-pipeline',
-                stageName='InvokeLambdaBeforeApproval',
-                actionName='Approval',
-                result={
-                    'summary': 'Rejected from Slack',
-                    'status': 'Rejected'
-                },
-                token=approval_token
-            )
-
+    # それ以外の場合
+    else:
         return {
             'statusCode': 200,
             'body': 'Done'
         }
-
-    return {
-        'statusCode': 200,
-        'body': 'Done'
-    }
