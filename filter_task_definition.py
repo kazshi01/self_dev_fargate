909fa98345778431a934698d3a24e20c511886f0
kazshi01 <blumedio0619@gmail.com>
2023-09-10 20:42:30 +0900

    test

--- filter_task_definition.py
+++ filter_task_definition.py
@@ -5,18 +5,23 @@ def main(input_file, output_file):
     with open(input_file, 'r') as f:
         data = json.load(f)
 
-    filtered_data = {
-        "family": data.get("family"),
-        "taskRoleArn": data.get("taskRoleArn"),
-        "executionRoleArn": data.get("executionRoleArn"),
-        "networkMode": data.get("networkMode"),
-        "containerDefinitions": data.get("containerDefinitions"),
-        "volumes": data.get("volumes"),
-        "placementConstraints": data.get("placementConstraints"),
-        "requiresCompatibilities": data.get("requiresCompatibilities"),
-        "cpu": data.get("cpu"),
-        "memory": data.get("memory"),
-    }
+    filtered_data = {}
+    keys = [
+        "family", 
+        "taskRoleArn", 
+        "executionRoleArn", 
+        "networkMode", 
+        "containerDefinitions", 
+        "volumes", 
+        "placementConstraints", 
+        "requiresCompatibilities", 
+        "cpu", 
+        "memory"
+    ]
+
+    for key in keys:
+        if key in data:
+            filtered_data[key] = data[key]
 
     with open(output_file, 'w') as f:
         json.dump(filtered_data, f)
